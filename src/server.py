d1028ad35dbb22b5c804f6bcfba9f77f7fd0fc33
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-13 15:48:58 -0800

    adding functions to inform client of a good server connection and bad server connection.

--- src/server.py
+++ src/server.py
@@ -2,6 +2,7 @@
 
 
 from __future__ import unicode_literals
+from sys import version_info
 import socket
 
 
@@ -12,7 +13,7 @@ def server():
         socket.SOCK_STREAM,
         socket.IPPROTO_TCP
     )
-    address = ('127.0.0.1', 5016)
+    address = ('127.0.0.1', 5017)
     server.bind(address)
 
     server.listen(1)
@@ -30,6 +31,7 @@ def server():
                 print('Recieved: ', part)
 
             print("Message sent...")
+            conn.sendall(server_good_connection().encode('utf8'))
             conn.sendall(response.encode('utf8'))
             conn, addr = server.accept()
 
@@ -37,8 +39,27 @@ def server():
             print('\nClosing echo server...')
             break
 
+        except StandardError:
+            conn.sendall(server_bad_connection().encode('utf8'))
+            break        
     conn.close()
     server.close()
+
+
+def server_good_connection():
+    """Send a HTTP response to client."""
+    message = 'HTTP/1.1 200 OK\n'
+    if version_info[0] == 2:
+        message = message.decode('utf-8')
+    return message
+
+
+def server_bad_connection():
+    """Send a HTTP response to client if bad request."""
+    message = 'HTTP/1.1 500 Internal Server Error\n'
+    if version_info[0] == 2:
+        message = message.decode('utf-8')
+    return message
 
 
 if __name__ == '__main__':
