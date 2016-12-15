7ee4e33a4eec7f8744f2dcdd5b6aa38c16ebcf1c
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-14 21:21:55 -0800

    refactored server() fcn to include response codes and reproduce appropriate error.

--- src/server.py
+++ src/server.py
@@ -31,17 +31,24 @@ def server():
                 client_request += part.decode('utf8')
 
             print(client_request)
-            conn.sendall(response_ok().encode('utf8'))
-            conn.sendall(client_request.encode('utf8'))
+            try:
+                parse_request(client_request)
+                conn.sendall(response_ok())
+            except SyntaxError:
+                conn.sendall(response_error('400'))
+            except NameError:
+                conn.sendall(response_error('405'))
+            except ValueError:
+                conn.sendall(response_error('505'))
+            except TypeError:
+                conn.sendall(response_error('404'))
+
             conn, addr = server.accept()
 
         except KeyboardInterrupt:
             print('\nClosing echo server...')
             break
 
-        # except StandardError:
-        #     conn.sendall(server_bad_connection().encode('utf8'))
-        #     break
     conn.close()
     server.close()
 
@@ -52,7 +59,7 @@ def response_ok():
     if version_info[0] == 2:
         message = message.decode('utf-8')
     message += u"\r\n"
-    return message
+    return message.encode('utf8')
 
 
 def response_error(exception):
@@ -61,30 +68,40 @@ def response_error(exception):
         '405': 'Method Not Allowed\n',
         '400': 'Bad Request\n',
         '505': 'HTTP Version Not Supported\n',
-        '500': 'Internal Server Error\n'
+        '500': 'Internal Server Error\n',
+        '404': 'Not Found\n'
     }
     if exception in error_codes:
         message = exception + ' ' + error_codes[exception]
         if version_info[0] == 2:
             message = message.decode('utf-8')
     message += u"\r\n"
-    return message
+    return message.encode('utf8')
 
 
 def parse_request(header):
     """Check header from client."""
+    """#TODO: What if there's a body after?"""
     header = header.split(' ', 4)
+    print(header)
 
     try:
-        method, uri, protocol, host = header
+        method, uri, protocol, address, port = header
+        # print("Method: " + method)
+        # print(type(method))
+        # print("uri: " + uri)
+        # print("protocol: " + protocol)
+        # print(protocol[:9])
+        # print("address: " + address)
+        # print("port: " + port)
 
     except ValueError:
         raise SyntaxError
-    if method is not 'GET':
+    if method != 'GET':
         raise NameError
-    elif protocol[:9] is not 'HTTP/1.1':
+    elif protocol[:8] != 'HTTP/1.1':
         raise ValueError
-    elif '127.0.0.1' not in host and str(PORT_NUMBER) not in host:
+    elif '127.0.0.1' not in address and str(PORT_NUMBER) not in port:
         raise TypeError
     return uri
 
