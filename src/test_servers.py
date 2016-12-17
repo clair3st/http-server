f0cc0bbd3b7b16f7c8d0a0192ca78f09aa348ee8
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-17 11:03:21 -0800

    removed commented out code

--- src/test_servers.py
+++ src/test_servers.py
@@ -3,9 +3,12 @@
 
 import pytest
 from server import PORT_NUMBER, ADDRESS
-# import sys
 
-HEADER = 'GET /src/server.py HTTP/1.1\r\n Host: {}:{}\r\n\r\n'.format(ADDRESS, PORT_NUMBER)
+
+HEADER = 'GET /src/server.py HTTP/1.1\r\n Host: {}:{}\r\n\r\n'.format(
+    ADDRESS,
+    PORT_NUMBER
+)
 
 ECHO_MESSAGES = [
     'Yo',
@@ -13,18 +16,29 @@ ECHO_MESSAGES = [
     'This has sixteen',
 ]
 
-# CLIENT_MESSAGES = [
-#     ['GET /index.html HTTP/1.1<CRLF> Host: {}:{}<CRLF><CRLF>'.format(ADDRESS, PORT_NUMBER),
-#      "HTTP/1.1 200 OK\n"],
-#     ['GET /src/server.py HTTP/1.1<CRLF> {}:{}<CRLF><CRLF>'.format(ADDRESS, PORT_NUMBER),
-#      "404 Not Found\n"],
-#     ['PUT /src/server.py HTTP/1.1<CRLF> Host: {}:{}<CRLF><CRLF>'.format(ADDRESS, PORT_NUMBER),
-#      "405 Method Not Allowed\n"],
-#     ['/src/server.py HTTP/1.1<CRLF> Host: {}:{}<CRLF><CRLF>'.format(ADDRESS, PORT_NUMBER),
-#      "400 Bad Request\n"],
-#     ['GET /src/server.py HTTP/1.0<CRLF> Host: {}:{}<CRLF><CRLF>'.format(ADDRESS, PORT_NUMBER),
-#      "505 HTTP Version Not Supported\n"],
-# ]r
+CLIENT_MESSAGES = [
+    ['GET /src/server.py HTTP/1.1<\r\n> {}:{}<\r\n><\r\n>'.format(
+        ADDRESS,
+        PORT_NUMBER
+    ),
+        "404 Not Found\n"],
+    ['PUT /src/server.py HTTP/1.1<\r\n> Host: {}:{}<\r\n><\r\n>'.format(
+        ADDRESS,
+        PORT_NUMBER
+    ),
+        "405 Method Not Allowed\n"],
+    ['/src/server.py HTTP/1.1<\r\n> Host: {}:{}<\r\n><\r\n>'.format(
+        ADDRESS,
+        PORT_NUMBER
+    ),
+        "400 Bad Request\n"],
+    ['GET /src/server.py HTTP/1.0<\r\n> Host: {}:{}<\r\n><\r\n>'.format(
+        ADDRESS,
+        PORT_NUMBER
+    ),
+        "505 HTTP Version Not Supported\n"],
+]
+
 ERROR_CODES = [
     ['405', b'405 Method Not Allowed\n\r\n\r\n'],
     ['400', b'400 Bad Request\n\r\n\r\n'],
@@ -36,12 +50,21 @@ ERROR_CODES = [
 HEADER_ERRORS = [
     ['GET /src/server.py HTTP/1.1\r\n Host: {}:80<\r\n><\r\n>'.format(ADDRESS),
      TypeError],
-    ['PUT /src/server.py HTTP/1.1\r\n Host: {}:{}<\r\n><\r\n>'.format(ADDRESS, PORT_NUMBER),
-     NameError],
-    ['/src/server.py HTTP/1.1<\r\n> Host: {}:{}<\r\n><\r\n>'.format(ADDRESS, PORT_NUMBER),
-     SyntaxError],
-    ['GET /src/server.py HTTP/1.0<\r\n> Host: {}:{}<\r\n><\r\n>'.format(ADDRESS, PORT_NUMBER),
-     ValueError],
+    ['PUT /src/server.py HTTP/1.1\r\n Host: {}:{}<\r\n><\r\n>'.format(
+        ADDRESS,
+        PORT_NUMBER
+    ),
+        NameError],
+    ['/src/server.py HTTP/1.1<\r\n> Host: {}:{}<\r\n><\r\n>'.format(
+        ADDRESS,
+        PORT_NUMBER
+    ),
+        SyntaxError],
+    ['GET /src/server.py HTTP/1.0<\r\n> Host: {}:{}<\r\n><\r\n>'.format(
+        ADDRESS,
+        PORT_NUMBER
+    ),
+        ValueError],
 ]
 
 URI = [
@@ -60,23 +83,7 @@ It is three lines long.
     ['/webroot/a_web_page.html', b"""<!DOCTYPE html>\n<html>\n<body>\n\n<h1>Code Fellows</h1>\n\n<p>A fine place to learn Python web programming!</p>\n\n</body>\n</html>\n\n""", 'text/html'],
     ['/webroot/make_time.py', b'''#!/usr/bin/env python\n\n"""\nmake_time.py\n\nsimple script that returns and HTML page with the current time\n"""\n\nimport datetime\n\ntime_str = datetime.datetime.now().isoformat()\n\nhtml = """\n<http>\n<body>\n<h2> The time is: </h2>\n<p> %s <p>\n</body>\n</http>\n""" % time_str\n\nprint(html)\n''', 'text/x-python'],
     ['/webroot/images', '''<a src=JPEG_example.jpg>JPEG_example.jpg</a> <a src=sample_1.png>sample_1.png</a> <a src=Sample_Scene_Balls.jpg>Sample_Scene_Balls.jpg</a>''', 'text/html'],
-    # ['webroot/images/JPEG_example.jpg', """some binary/ byte string TBD""", 'image/jpg']
 ]
-# @pytest.mark.parametrize("result", ECHO_MESSAGES)
-# def test_message_completion(result):
-#     """Test table of potential messages for receipt and transmission."""
-#     from client import client
-#     assert client(result) == result
-
-
-# def test_message_unicode():
-#     """Test for unicode messages."""
-#     from client import client
-#     msg = 'CÅT'
-#     if sys.version_info[0] == 2:
-#         assert client(msg) == msg.decode('utf8')
-#     else:
-#         assert client(msg) == msg
 
 
 def test_response_ok():
@@ -106,11 +113,11 @@ def test_parse_request_correct():
     assert parse_request(HEADER) == '/src/server.py'
 
 
-# @pytest.mark.parametrize("message, result", CLIENT_MESSAGES)
-# def test_server_loop(message, result):
-#     """Test that server loop functions as expected."""
-#     from client import client
-#     assert client(message) == result
+@pytest.mark.parametrize("message, result", CLIENT_MESSAGES)
+def test_server_loop(message, result):
+    """Test that server loop functions as expected."""
+    from client import client
+    assert client(message).decode('utf8') == result
 
 
 @pytest.mark.parametrize("fname, body, content_type", FILE_TABLE)
